fe9622583657c20779916dab9dfccd1a6b5d9a49
Eric <esquared722@gmail.com>
2020-05-25 17:07:52 -0400

    Finished board.py
    As of this commit, board.py has been completed according to how I saw it
    should be implemented. There may be edits in the future, but this seems
    as a could ending point for this file.

--- src/game/board.py
+++ src/game/board.py
@@ -2,23 +2,26 @@ class Board:
     ''' Traditional Tic-Tac-Toe Board '''
 
     def __init__(self):
-        self.pieces = []
+        self.board = [[' ' for x in range(3)] for y in range(3)]
     
     def __str__(self):
         row = '-----------'
 
         for i in range(3):
             for j in range(3):
-                print(self.pieces[i][j], end = '|')
+                print(self.board[i][j], end = '|')
             print(row)
     
     
-    def placePiece(self, piece, pos):
+    def placePiece(self, piece, posX, posY):
         ''' Places piece object in given position on the board, simulating a move'''
-        # TODO
-        return;
+        self.board[posY][posX] = piece
     
     def clearBoard(self):
-        # TODO
-        return;
-
+        ''' Clears board of all pieces '''
+        for i in range(3):
+            for j in range(3):
+                board[i][j] = ' ';
+    
+    def getBoard(self):
+        return self.board
