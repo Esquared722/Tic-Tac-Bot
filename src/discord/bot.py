a95979c9630a62058e12b6786f69a59edb9b6fbe
Eric <esquared722@gmail.com>
2020-06-08 01:20:15 -0400

    Made more progress on implementing the main function of the bot. The first command to be completed will be ttt I have created a dictionary to allow for jumps to be used. Next step is to integrate code made for the backend

--- src/discord/bot.py
+++ src/discord/bot.py
@@ -3,7 +3,7 @@ import discord, json
 with open("./config.json", "r") as read_file:
     config = json.load(read_file)
 
-token = config["token"]
+token, prefix, cID = config["token"], config["prefix"], config["channel_id"]
 
 client = discord.Client()
 
@@ -13,10 +13,20 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == client.user or not message.content.startswith(prefix):
+        return
+    parameters = message.content.split(" ")
+    try:
+        commands = {'ttt': playGame} # dictionary of bot commands
+        await commands[parameters[0][1:]](message.channel, message.author, parameters[1])
+    except IndexError:
+        await commands[parameters[0][1:]](message.author) # game against a bot
+    except KeyError:
+        await message.channel.send('Invalid Command!')
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+async def playGame(channel, p1, p2=''):
+    await channel.send('This is a game between {} and {}.'.format(p1, 'a bot' if p2 is '' else p2))
+    # TODO implement rest of a standard game, by using previously made code
 
 client.run(token)
